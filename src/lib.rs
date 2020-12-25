776af0b4c99dca8c3d105a161573d64cc6d729ff
Øystein Hovind <hovindo@gmail.com>
2020-12-25 18:14:09 +0100

    Slightly less initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -17,19 +17,32 @@ const fn choose(n: usize, mut r: usize) -> usize {
     return ans;
 }
 
-pub struct Multivector<T, const N: usize> where
-[(); 1 << N]: Sized,
-{
-    data: [T; 1 << N],
+#[derive(PartialEq, Eq)]
+pub struct Clifford {
+    positive: usize,
+    negative: usize,
+    zero: usize
 }
 
-impl<T, const N: usize> Multivector<T, N> where
-[(); 1 << N]: Sized,
+impl Clifford {
+    const fn dim(self) -> usize {
+        self.positive + self.negative + self.zero
+    }
+}
+
+pub struct Multivector<T, const C: Clifford> where
+[(); 1 << C.dim()]: Sized,
 {
-    pub fn grade<const G: usize>(&self) -> &[T; choose(N, G)] {
+    data: [T; 1 << C.dim()],
+}
+
+impl<T, const C: Clifford> Multivector<T, C> where
+[(); 1 << C.dim()]: Sized,
+{
+    pub fn grade<const G: usize>(&self) -> &[T; choose(C.dim(), G)] {
         let offset = 1 << G;
         unsafe {
-            std::ptr::read(&self.data[offset..] as *const [T] as *const &[T; choose(N, G)])
+            std::ptr::read(&self.data[offset..] as *const [T] as *const &[T; choose(C.dim(), G)])
         }
     }
 }
