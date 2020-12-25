641ed5e031964e2e42642250ead3c813466df52a
Øystein Hovind <hovindo@gmail.com>
2020-12-25 18:53:12 +0100

    Refactor some minor details

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,10 @@ const fn choose(n: usize, mut r: usize) -> usize {
         ans /= i;
         i += 1;
     }
-
-    return ans;
+    ans
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Clifford {
     positive: usize,
     negative: usize,
@@ -31,6 +30,18 @@ impl Clifford {
     const fn size(self) -> usize {
         self.dim() << 1
     }
+    const fn len(self, grade: usize) -> usize {
+        choose(self.dim(), grade)
+    }
+    const fn offset(self, grade: usize) -> usize {
+        let mut g = 0usize;
+        let mut ans = 0usize;
+        while g < grade {
+            ans += self.len(g);
+            g += 1;
+        }
+        ans
+    }
 }
 
 pub struct Multivector<T, const C: Clifford> where
@@ -42,10 +53,10 @@ pub struct Multivector<T, const C: Clifford> where
 impl<T, const C: Clifford> Multivector<T, C> where
 [(); C.size()]: Sized,
 {
-    pub fn grade<const G: usize>(&self) -> &[T; choose(C.dim(), G)] {
+    pub fn grade<const G: usize>(&self) -> &[T; C.len(G)] {
         let offset = 1 << G;
         unsafe {
-            std::ptr::read(&self.data[offset..] as *const [T] as *const &[T; choose(C.dim(), G)])
+            std::ptr::read(&self.data[offset..] as *const [T] as *const &[T; C.len(G)])
         }
     }
 }
